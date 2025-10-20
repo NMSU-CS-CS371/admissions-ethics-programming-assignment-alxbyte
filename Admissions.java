fcff7e89f1c33d2332bd9affd8ac1d45909f7b30
Alexis Quintero <alxq05@nmsu.edu>
2025-10-20 14:44:39 -0600

    answers in the readme

--- Admissions.java
+++ Admissions.java
@@ -6,11 +6,11 @@ public class Admissions {
     // Blind model (only academic/performance factors)
     public static double blindScore(Applicant app) {
         double score = 0.0;
-        score += (app.gpa / 4.0) * 0.4;     // GPA normalized
+        score += (app.gpa / 4.0) * 0.35;     // GPA normalized
         score += (app.test / 1600.0) * 0.3;  // Test score normalized
-        score += app.extra * 0.1;
-        score += app.essay * 0.1;
-        score += app.rec * 0.1;
+        score += app.extra * 0.05;
+        score += app.essay * 0.15;
+        score += app.rec * 0.15;
         return score; // final score between 0 and 1
     }
 
@@ -19,10 +19,13 @@ public class Admissions {
         double score = blindScore(app);
 
         if (app.income < 40000) score += 0.05;     // low-income boost
-        if (app.firstGen) score += 0.05;           // first-generation bonus
-        if (app.disability) score += 0.03;         // accessibility consideration
-        if (app.legacy) score += 0.02;             // legacy advantage
-        if (app.local) score += 0.03;              // local preference
+        if (app.firstGen) score += 0.04;           // first-generation bonus
+        if (app.disability) score += 0.05;         // accessibility consideration
+        if (app.legacy) score -= 0.1;             // legacy advantage
+        if (app.local) score += 0.0;              // local preference
+        if (app.ethnicity != "White") score += 0.02; //ethnicity preference 
+        
+
         return Math.min(score, 1.0);               // cap score at 1.0
     }
 }
